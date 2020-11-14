ba5fd14ff5e90e78a8d049ce4d21ea5f48105348
DizZy <disk.nitrome@gmail.com>
2020-11-15 03:52:39 +0700

    Create member list test

--- src/views/MemberManage.vue
+++ src/views/MemberManage.vue
@@ -3,8 +3,27 @@
   <div>
     <!-- Use NavBar -->
     <nav-bar></nav-bar>
-
     <h1> {{ title }} </h1>
+
+    <div id="employee-table">
+      <table class="table table-bordered">
+        <thead class="thead-dark">
+          <tr>
+            <th>Employee name</th>
+            <th>Employee email</th>
+            <th>Employee position</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="employee in employees" :key="employee.id">
+            <td> {{ employee.name }} </td>
+            <td> {{ employee.email }} </td>
+            <td> {{ employee.position }} </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
   </div>
 
 </template>
@@ -15,7 +34,22 @@ import NavBar from '../components/NavBar.vue'
 export default {
   data() {
     return {
-      title: 'Member Management'
+      title: 'Member management',
+      //member lists testing
+      employees: [
+        {
+          id: 1,
+          name: 'Steve',
+          email: 'steve@email.com',
+          position: 'Owner'
+        },
+        {
+          id: 2,
+          name: 'Bobby',
+          email: 'bobby@gmail.com',
+          position: 'Sale staff'
+        }
+        ]
     }
   },
   methods: {
@@ -27,4 +61,11 @@ export default {
 </script>
 
 <style>
+  table, tr, td { 
+    border: 1px solid black;
+  }
+  h1 {
+    text-align: center;
+    margin: 1 auto;
+  }
 </style>
