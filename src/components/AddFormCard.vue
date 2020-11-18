ff0bfb54e54ada82475c56ecb96729065f4fe427
Nuttawatt Sunthornsatienkul <nuttawatt.s@ku.th>
2020-11-18 22:12:25 +0700

    Add function for adding product and delete product

---
+++ src/components/AddFormCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <b-button v-b-modal.modal-add-product>Add Product</b-button>
+        <b-modal id="modal-add-product" title="Add Product" @ok="addPro">
+          <div>
+            ID
+            <br />
+            <input v-model="addProduct.id" class="validate" placeholder="ID">
+          </div>
+          <div>
+            Name
+            <br />
+            <input v-model="addProduct.name" class="validate" placeholder="Name">
+          </div>
+          <div>
+            Description
+            <br />
+            <input v-model="addProduct.description" class="validate" placeholder="Description">
+          </div>
+          <div>
+            Quantity
+            <br />
+            <input v-model.number="addProduct.qty" class="validate" placeholder="Quantity">
+          </div>
+        </b-modal>
+  </div>
+</template>
+
+<script>
+import { firestore } from "firebase";
+import { db, productCol } from "../firebase";
+export default {
+    props: ['addProduct', 'addPro'],
+};
+</script>
+
+<style>
+</style>
