8e57b08d6698c1212394f92db4651a07032c1102
Ratanon Treewannakul <rattanon.tr@ku.th>
2020-11-20 07:48:38 +0700

    Finish main function

--- src/components/logTable.vue
+++ src/components/logTable.vue
@@ -5,24 +5,29 @@
             <tr>
                 <th>ID</th>
                 <th>Date Time</th>
-                <th>Price</th>
+                <th v-if="oderin != 'Stock_log'">Price</th>
                 <th>Detail</th>
             </tr>
-            <template v-for="ord in SellOrd" >
-              <tr v-if="ord[0].date >= DateRange[0] && ord[0].date <= DateRange[1]" :key="ord[0].o_id">
-                <td> {{ ord[0].o_id }} </td>
-                <td> {{ ord[0].date }} </td>
-                <td> {{ ord[0].o_total }} </td>
-                <td > <button @click="show(ord[1])">Detail</button></td>
+            <template v-for="ord in Ord" >
+              <tr v-if="ord.date >= DateRange[0] && ord.date <= DateRange[1]" :key="ord.o_id">
+                <td> {{ ord.o_id }} </td>
+                <td> {{ ord.date }} </td>
+                <td  v-if="oderin != 'Stock_log'"> {{ ord.o_total }} </td>
+                <td > <button @click="show(ord.o_id)">Detail</button></td>
              </tr>
               </template>
         </table>
         </div>
-        <modal name="detail-popup" transition="pop-out" :width="400" :focus-trap="true" :height="400">
-            <ul v-for="(dets,index) in recp" :key="index">
-                <li>{{ dets.p_id }}</li>
-                <li>{{ dets.o_amount }}</li>
+        <modal name="detail-popup" transition="pop-out" :width="400" :focus-trap="true" :height="400" >
+            <template v-for="(det,index) in DeOrd">
+            <ul v-if="det.o_id == recp" :key="index">
+                <li>{{ det.p_id }}</li>
+                <li>{{ det.o_amount }}</li>
+                <li  v-if="oderin != 'Stock_log'">{{ det.o_price }}</li>
+                <a>___________________</a>
             </ul>
+            </template>
+            <button @click="hide()">close</button>
         </modal>
     </div>
 </template>
@@ -33,13 +38,14 @@ export default {
         return{
         DateRange: this.Dainit,
         oderin: this.ODrinit,
-        SellOrd: this.SOinit,
-        DeliOrd: this.DOinit,
-        recp:[]
+        Ord: this.SOinit,
+        DeOrd: this.DOinit,
+        recp:'',
     }
     },
     methods:{
         show(rec){
+            console.log(rec)
             this.recp = rec
             // console.log(rec)
             // console.log(rec)
@@ -72,5 +78,4 @@ export default {
 </script>
 
 <style>
-
 </style>
